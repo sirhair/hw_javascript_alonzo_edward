8851e16b832093e9ff40bd5f3a2740ab3b9e90d2
Edward Alonzo <edward@thenewe.com>
2015-01-31 14:44:41 -0600

    added presidents object

---
+++ js/main.js
@@ -0,0 +1,44 @@
+var presidents =
+[
+	{
+		firstName:"Barack",
+		lastName:"Obama",
+		termLength:"8",
+		party:"Democrat",
+		yearsOfPresidency:"2009-2016"
+
+	},
+	{
+		firstName:"George W.",
+		lastName:"Bush",
+		termLength:"8",
+		party:"Republican",
+		yearsOfPresidency:"2001-2009"
+
+	},
+	{
+		firstName:"Bill",
+		lastName:"Clinton",
+		termLength:"8",
+		party:"Democrat",
+		yearsOfPresidency:"1993-2001"
+
+	},
+	{
+		firstName:"George H. W.",
+		lastName:"Bush",
+		termLength:"4",
+		party:"Republican",
+		yearsOfPresidency:"1989-1993"
+
+	},
+	{
+		firstName:"Ronald",
+		lastName:"Regan",
+		termLength:"8",
+		party:"Republican",
+		yearsOfPresidency:"1981-1989"
+
+	}
+];
+console.log(presidents[2].firstName,presidents[2].lastName);
