baaa90ef392491f1061c56fe11f74490102895bb
Richard Kalehoff <richardkalehoff@gmail.com>
2015-02-02 21:43:36 -0500

    give code more spacing for readability

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 function howdy (){
-	console.log("Edward Alonzo says hi!");
+    console.log("Edward Alonzo says hi!");
 };
 
 howdy();
 
 var numba = function(string){
-	if (string.length<7){
-		console.log("What a short little word!");
-	}else if(string.length>7){
-		console.log("I'm sorry but that's too many to count.");
-	}else if(string.length==7){
-		console.log("7, what a perfect choice!");
-	}else{
-		console.log("Something's wrong... I think I smell burning.");
-	}
+    if (string.length<7) {
+        console.log("What a short little word!");
+    } else if (string.length>7) {
+        console.log("I'm sorry but that's too many to count.");
+    } else if (string.length==7) {
+        console.log("7, what a perfect choice!");
+    } else{
+        console.log("Something's wrong... I think I smell burning.");
+    }
 };
 
 numba("7");
@@ -21,6 +21,8 @@ numba("seventy");
 numba("university");
 
 function inception(callback,value){
-	callback(value);
+    callback(value);
 }
-inception(function logit(title){console.log(title + " is fantastic movie.");}, "Finding Nemo");
+inception( function logit(title) {
+    console.log(title + " is fantastic movie.");
+}, "Finding Nemo" );
